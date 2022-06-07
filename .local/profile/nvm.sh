454b85781239127e4b5e680740df45f2c90553af
Henrique Delazeri <hdelazeri@hdelazeri.dev>
2022-06-07 19:30:42 -0300

    Add nvm to path

---
+++ .local/profile/nvm.sh
@@ -0,0 +1,6 @@
+if [ -d "/usr/share/nvm" ]; then
+  [ -z "$NVM_DIR" ] && export NVM_DIR="$HOME/.nvm"
+  source /usr/share/nvm/nvm.sh
+  source /usr/share/nvm/bash_completion
+  source /usr/share/nvm/install-nvm-exec
+fi
